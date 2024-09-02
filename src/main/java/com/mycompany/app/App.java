387e63af75b300c66f37860931ce13fe55e50f06
Steve Chien <steve@productlab.com>
2024-09-01 22:32:37 -0400

    add some dummy test code

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,8 +5,25 @@
 
 package com.mycompany.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
+class Fetch {
+    void fetch(List<? super Float> l) {
+        l.add(100.0f);
+        l.add(200.0f);
+    }
+}
+
 public class App {
     public static void main(String[] args) {
+        List<Number> l = new ArrayList<>();
+        Fetch f = new Fetch();
+        f.fetch(l);
+        for (Number n : l) {
+            System.out.println(n.toString());
+        }
+
         System.out.println("Hello Remote World!");
     }
 }
